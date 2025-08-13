185ef2454ae0df087f27b1de366cee168cabb560
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2025-08-13 12:58:02 -0700

    Obliterate the lambdas

--- src/main/java/team/lodestar/lodestone/systems/rendering/vertexconsumer/MultiVertexConsumer.java
+++ src/main/java/team/lodestar/lodestone/systems/rendering/vertexconsumer/MultiVertexConsumer.java
@@ -3,57 +3,63 @@ package team.lodestar.lodestone.systems.rendering.vertexconsumer;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 /**
  * A {@link VertexConsumer} that forwards all calls to multiple other {@link VertexConsumer}s.
  * <p>Useful for rendering the same geometry to multiple buffers.</p>
  */
 @SuppressWarnings("unused")
 public class MultiVertexConsumer implements VertexConsumer {
-    private final List<VertexConsumer> consumers;
+    private final VertexConsumer[] consumers;
 
     public MultiVertexConsumer(VertexConsumer... consumers) {
-        this(List.of(consumers));
-    }
-
-    public MultiVertexConsumer(List<VertexConsumer> consumers) {
         this.consumers = consumers;
     }
 
     @Override
     public @NotNull VertexConsumer addVertex(float v, float v1, float v2) {
-        consumers.forEach(consumer -> consumer.addVertex(v, v1, v2));
+        for (VertexConsumer consumer : consumers) {
+            consumer.addVertex(v, v1, v2);
+        }
         return this;
     }
 
     @Override
     public @NotNull VertexConsumer setColor(int i, int i1, int i2, int i3) {
-        consumers.forEach(consumer -> consumer.setColor(i, i1, i2, i3));
+        for (VertexConsumer consumer : consumers) {
+            consumer.setColor(i, i1, i2, i3);
+        }
         return this;
     }
 
     @Override
     public @NotNull VertexConsumer setUv(float v, float v1) {
-        consumers.forEach(consumer -> consumer.setUv(v, v1));
+        for (VertexConsumer consumer : consumers) {
+            consumer.setUv(v, v1);
+        }
         return this;
     }
 
     @Override
     public @NotNull VertexConsumer setUv1(int i, int i1) {
-        consumers.forEach(consumer -> consumer.setUv1(i, i1));
+        for (VertexConsumer consumer : consumers) {
+            consumer.setUv1(i, i1);
+        }
         return this;
     }
 
     @Override
     public @NotNull VertexConsumer setUv2(int i, int i1) {
-        consumers.forEach(consumer -> consumer.setUv2(i, i1));
+        for (VertexConsumer consumer : consumers) {
+            consumer.setUv2(i, i1);
+        }
         return this;
     }
 
     @Override
     public @NotNull VertexConsumer setNormal(float v, float v1, float v2) {
-        consumers.forEach(consumer -> consumer.setNormal(v, v1, v2));
+        for (VertexConsumer consumer : consumers) {
+            consumer.setNormal(v, v1, v2);
+        }
         return this;
     }
 }
