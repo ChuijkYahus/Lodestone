121d858a455fb5882a616189843f96140bf81444
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2025-08-14 20:44:12 +0200

    Update LodestoneShaderRegistry.java

--- src/main/java/team/lodestar/lodestone/systems/rendering/shader/LodestoneShaderRegistry.java
+++ src/main/java/team/lodestar/lodestone/systems/rendering/shader/LodestoneShaderRegistry.java
@@ -1,5 +1,7 @@
 package team.lodestar.lodestone.systems.rendering.shader;
 
+import com.mojang.blaze3d.vertex.*;
+import net.minecraft.resources.*;
 import net.neoforged.neoforge.client.event.RegisterShadersEvent;
 import team.lodestar.lodestone.LodestoneLib;
 
@@ -12,6 +14,10 @@ public class LodestoneShaderRegistry {
 
     public LodestoneShaderRegistry(String modId) {
         this.modId = modId;
+    }
+    
+    public ShaderHolder register(String id, VertexFormat format) {
+        return register(new ShaderHolder(ResourceLocation.fromNamespaceAndPath(modId, id), format));
     }
 
     public <T extends LodestoneShader> T register(T shader) {
