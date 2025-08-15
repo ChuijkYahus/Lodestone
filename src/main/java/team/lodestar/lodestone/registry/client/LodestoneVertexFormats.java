6947269bea9ad10abb400b9b74c9e3f9a4bca56d
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2025-08-14 17:02:03 -0700

    disgusting vertex format

--- src/main/java/team/lodestar/lodestone/registry/client/LodestoneVertexFormats.java
+++ src/main/java/team/lodestar/lodestone/registry/client/LodestoneVertexFormats.java
@@ -4,19 +4,6 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormatElement;
 
 public class LodestoneVertexFormats {
-    public static VertexFormatElement TANGENT = VertexFormatElement.register(VertexFormatElement.findNextId(),0, VertexFormatElement.Type.BYTE, VertexFormatElement.Usage.NORMAL, 3);
-    public static VertexFormatElement BITANGENT = VertexFormatElement.register(VertexFormatElement.findNextId(),0, VertexFormatElement.Type.BYTE, VertexFormatElement.Usage.NORMAL, 3);
-
-    //Scary!!
-    public static final VertexFormat POSITION_COLOR_TEX_LIGHTMAP_NORMAL_TANGENT_BITANGENT_UVOFFSET = VertexFormat.builder()
-            .add("Position", VertexFormatElement.POSITION) // 3 floats 3*4
-            .add("Color", VertexFormatElement.COLOR) // 4 bytes 4*1
-            .add("UV0", VertexFormatElement.UV0) // 2 floats 2*4
-            .add("UV2", VertexFormatElement.UV2) // 2 shorts 2*2
-            .add("Normal", VertexFormatElement.NORMAL) // 3 bytes 3*1
-            .add("Tangent", TANGENT) // 3 bytes 3*1
-            .add("Bitangent", BITANGENT) // 3 bytes 3*1
-            .add("UVOffset", VertexFormatElement.UV) // 2 shorts 2*2
-            .padding(23) //41 total
-            .build();
+    public static VertexFormatElement TANGENT = VertexFormatElement.register(VertexFormatElement.findNextId(), 0, VertexFormatElement.Type.BYTE, VertexFormatElement.Usage.NORMAL, 3);
+    public static VertexFormatElement BITANGENT = VertexFormatElement.register(VertexFormatElement.findNextId(), 0, VertexFormatElement.Type.BYTE, VertexFormatElement.Usage.NORMAL, 3);
 }
