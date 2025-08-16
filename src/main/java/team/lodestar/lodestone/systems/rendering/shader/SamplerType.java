2dd36588ef581da2acd35864c6ec56ba37e6b61f
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2025-08-16 15:49:25 -0700

    Cubemaps

--- src/main/java/team/lodestar/lodestone/systems/rendering/shader/SamplerType.java
+++ src/main/java/team/lodestar/lodestone/systems/rendering/shader/SamplerType.java
@@ -1,13 +1,12 @@
 package team.lodestar.lodestone.systems.rendering.shader;
 
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_1D;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL12.GL_TEXTURE_3D;
+import static org.lwjgl.opengl.GL20.*;
 
 public enum SamplerType {
     TEXTURE_1D("1d", GL_TEXTURE_1D),
     TEXTURE_2D("2d", GL_TEXTURE_2D),
-    TEXTURE_3D("3d", GL_TEXTURE_3D);
+    TEXTURE_3D("3d", GL_TEXTURE_3D),
+    TEXTURE_CUBE("cube", GL_TEXTURE_CUBE_MAP);
 
     final String type;
     final int glType;
