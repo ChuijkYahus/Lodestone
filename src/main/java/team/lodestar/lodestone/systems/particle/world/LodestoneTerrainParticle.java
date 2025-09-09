a6907b63483b9dad36b37a9d7420a97652783c71
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2025-09-09 12:21:48 +0200

    Update LodestoneTerrainParticle.java

--- src/main/java/team/lodestar/lodestone/systems/particle/world/LodestoneTerrainParticle.java
+++ src/main/java/team/lodestar/lodestone/systems/particle/world/LodestoneTerrainParticle.java
@@ -4,9 +4,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.*;
 import team.lodestar.lodestone.systems.particle.world.options.LodestoneTerrainParticleOptions;
-import team.lodestar.lodestone.systems.particle.world.behaviors.*;
 
 public class LodestoneTerrainParticle extends LodestoneWorldParticle {
 
@@ -17,39 +16,42 @@ public class LodestoneTerrainParticle extends LodestoneWorldParticle {
     public LodestoneTerrainParticle(ClientLevel world, LodestoneTerrainParticleOptions data, double x, double y, double z, double xd, double yd, double zd) {
         super(world, data, null, x, y, z, xd, yd, zd);
         this.blockPos = data.blockPos;
-        if (!data.blockState.is(Blocks.GRASS_BLOCK)) {
-            int i = Minecraft.getInstance().getBlockColors().getColor(data.blockState, world, data.blockPos, 0);
-            this.rCol *= (float) (i >> 16 & 255) / 255.0F;
-            this.gCol *= (float) (i >> 8 & 255) / 255.0F;
-            this.bCol *= (float) (i & 255) / 255.0F;
+        var state = data.blockState;
+        this.setSprite(Minecraft.getInstance().getBlockRenderer().getBlockModelShaper().getParticleIcon(state));
+        this.gravity = 1.0F;
+        this.rCol = 0.6F;
+        this.gCol = 0.6F;
+        this.bCol = 0.6F;
+        if (net.neoforged.neoforge.client.extensions.common.IClientBlockExtensions.of(state).areBreakingParticlesTinted(state, level, blockPos)) {
+            int i = Minecraft.getInstance().getBlockColors().getColor(state, level, blockPos, 0);
+            this.rCol *= (float)(i >> 16 & 0xFF) / 255.0F;
+            this.gCol *= (float)(i >> 8 & 0xFF) / 255.0F;
+            this.bCol *= (float)(i & 0xFF) / 255.0F;
         }
 
         this.quadSize /= 2.0F;
         this.uo = this.random.nextFloat() * 3.0F;
         this.vo = this.random.nextFloat() * 3.0F;
-
-        setSprite(Minecraft.getInstance().getBlockRenderer().getBlockModelShaper().getParticleIcon(data.blockState));
     }
-
 
     @Override
     public float getU0() {
-        return this.sprite.getU(((this.uo + 1.0F) / 4.0F * 16.0F));
+        return this.sprite.getU((this.uo + 1.0F) / 4.0F);
     }
 
     @Override
     public float getU1() {
-        return this.sprite.getU((this.uo / 4.0F * 16.0F));
+        return this.sprite.getU(this.uo / 4.0F);
     }
 
     @Override
     public float getV0() {
-        return this.sprite.getV((this.vo / 4.0F * 16.0F));
+        return this.sprite.getV(this.vo / 4.0F);
     }
 
     @Override
     public float getV1() {
-        return this.sprite.getV(((this.vo + 1.0F) / 4.0F * 16.0F));
+        return this.sprite.getV((this.vo + 1.0F) / 4.0F);
     }
 
     @Override
