a9683e4fbd5dad81af8f203455434bea8bd1dd8f
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2026-01-13 06:12:24 +0100

    Armor Model Simplified

--- src/main/java/team/lodestar/lodestone/systems/model/LodestoneArmorModel.java
+++ src/main/java/team/lodestar/lodestone/systems/model/LodestoneArmorModel.java
@@ -10,14 +10,16 @@ import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 
+import java.util.*;
+
 /**
  * A simple implementation of an armor tied humanoid model.
- * Designing these in blockbench is quite annoying, but from my personal experience it can be made simpler with the following steps;
- * The Root* of each individual part of your armor model should match in name to the ModelParts defined below. That way, the exported code will be really easy to connect to the LodestoneArmorModel utility.
- * The Pivot Points of each Actual Root* of your individual parts of the armor model should match to that of the player's model.
- * Export the model as a java file and paste what it gives you into {@link LodestoneArmorModel#createArmorModel}.
- * Remove the pose-offsets of any part that is directly tied to the player model and copies it's properties.
- * Hopefully you should be good to go after that? All of this is optional it's just to make the process easier. If you plan to use LodestoneArmorModel definitely join the discord and ask questions.
+ * Designing these in blockbench should be as simple as possible:
+ * Start with armor_boilerplate.bbmodel
+ * Do not change the pivot points of any bone already present in the boilerplate.
+ * Once you're done, Export the model as a java file and paste the geometry into {@link LodestoneArmorModel#createArmorModel}.
+ * Remove the generated part definitions of the Root* ModelParts
+ * Done
  */
 public class LodestoneArmorModel extends HumanoidModel<LivingEntity> {
     public EquipmentSlot slot;
@@ -26,44 +28,48 @@ public class LodestoneArmorModel extends HumanoidModel<LivingEntity> {
     public LodestoneArmorModel(ModelPart root) {
         super(root);
         this.root = root;
-        this.head = root.getChild("head");
-        this.body = root.getChild("body");
-        this.leggings = root.getChild("leggings");
-        this.leftArm = root.getChild("left_arm");
-        this.rightArm = root.getChild("right_arm");
-        this.leftLegging = root.getChild("left_legging");
-        this.rightLegging = root.getChild("right_legging");
-        this.leftFoot = root.getChild("left_foot");
-        this.rightFoot = root.getChild("right_foot");
+        this.head = getPart("head");
+        this.body = getPart("body");
+        this.leggings = getPart("leggings");
+        this.leftArm = getPart("left_arm");
+        this.rightArm = getPart("right_arm");
+        this.leftLegging = getPart("left_legging");
+        this.rightLegging = getPart("right_legging");
+        this.leftFoot = getPart("left_foot");
+        this.rightFoot = getPart("right_foot");
+    }
+
+    public ModelPart getPart(String name) {
+        return getPart(root, name);
     }
 
     public static PartDefinition createHumanoidAlias(MeshDefinition mesh) {
         PartDefinition root = mesh.getRoot();
-        root.addOrReplaceChild("body", new CubeListBuilder(), PartPose.ZERO);
-        root.addOrReplaceChild("leggings", new CubeListBuilder(), PartPose.ZERO);
         root.addOrReplaceChild("head", new CubeListBuilder(), PartPose.ZERO);
-        root.addOrReplaceChild("left_legging", new CubeListBuilder(), PartPose.ZERO);
-        root.addOrReplaceChild("left_foot", new CubeListBuilder(), PartPose.ZERO);
-        root.addOrReplaceChild("right_legging", new CubeListBuilder(), PartPose.ZERO);
-        root.addOrReplaceChild("right_foot", new CubeListBuilder(), PartPose.ZERO);
-        root.addOrReplaceChild("left_arm", new CubeListBuilder(), PartPose.ZERO);
+        root.addOrReplaceChild("body", new CubeListBuilder(), PartPose.ZERO);
         root.addOrReplaceChild("right_arm", new CubeListBuilder(), PartPose.ZERO);
+        root.addOrReplaceChild("left_arm", new CubeListBuilder(), PartPose.ZERO);
+        root.addOrReplaceChild("leggings", new CubeListBuilder(), PartPose.ZERO);
+        root.addOrReplaceChild("right_legging", new CubeListBuilder(), PartPose.ZERO);
+        root.addOrReplaceChild("left_legging", new CubeListBuilder(), PartPose.ZERO);
+        root.addOrReplaceChild("right_foot", new CubeListBuilder(), PartPose.ZERO);
+        root.addOrReplaceChild("left_foot", new CubeListBuilder(), PartPose.ZERO);
         return root;
     }
 
     public static LayerDefinition createArmorModel(ILodestoneArmorModelBuilder modelBuilder) {
         MeshDefinition mesh = HumanoidModel.createMesh(new CubeDeformation(0), 0);
         PartDefinition root = createHumanoidAlias(mesh);
+        PartDefinition head = root.getChild("head");
         PartDefinition body = root.getChild("body");
+        PartDefinition right_arm = root.getChild("right_arm");
+        PartDefinition left_arm = root.getChild("left_arm");
         PartDefinition leggings = root.getChild("leggings");
         PartDefinition right_legging = root.getChild("right_legging");
         PartDefinition left_legging = root.getChild("left_legging");
         PartDefinition right_foot = root.getChild("right_foot");
         PartDefinition left_foot = root.getChild("left_foot");
-        PartDefinition right_arm = root.getChild("right_arm");
-        PartDefinition left_arm = root.getChild("left_arm");
-        PartDefinition head = root.getChild("head");
-        return modelBuilder.createArmorLayer(mesh, root, body, leggings, right_legging, left_legging, right_foot, left_foot, right_arm, left_arm, head);
+        return modelBuilder.createArmorLayer(mesh, root, head, body, right_arm, left_arm, leggings, right_legging, left_legging, right_foot, left_foot);
     }
 
     @Override
@@ -104,7 +110,16 @@ public class LodestoneArmorModel extends HumanoidModel<LivingEntity> {
         rightFoot.copyFrom(rightLeg);
     }
 
+
+    public static ModelPart getPart(ModelPart root, String name) {
+        try {
+            return root.getChild(name);
+        } catch (Exception ignored) {
+            return new ModelPart(Collections.emptyList(), Collections.emptyMap());
+        }
+    }
+
     public interface ILodestoneArmorModelBuilder {
-        LayerDefinition createArmorLayer(MeshDefinition mesh, PartDefinition root, PartDefinition body, PartDefinition leggings, PartDefinition right_legging, PartDefinition left_legging, PartDefinition right_foot, PartDefinition left_foot, PartDefinition right_arm, PartDefinition left_arm, PartDefinition head);
+        LayerDefinition createArmorLayer(MeshDefinition mesh, PartDefinition root, PartDefinition head, PartDefinition body, PartDefinition right_arm, PartDefinition left_arm, PartDefinition leggings, PartDefinition right_legging, PartDefinition left_legging, PartDefinition right_foot, PartDefinition left_foot);
     }
 }
