32dfcdc90a6b367bc1c24fcaf997739595542d2c
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2026-02-18 14:21:13 +0100

    Update SoundHelper.java

--- src/main/java/team/lodestar/lodestone/helpers/SoundHelper.java
+++ src/main/java/team/lodestar/lodestone/helpers/SoundHelper.java
@@ -4,10 +4,20 @@ import net.minecraft.sounds.*;
 import net.minecraft.util.*;
 import net.minecraft.world.entity.*;
 
+import java.util.function.*;
+
 /**
  * A helper class containing various methods designed to play sound from a side independent context.
  */
 public class SoundHelper {
+
+    public static void playSoundRandomPitch(Entity target, Supplier<SoundEvent> soundEvent, float minPitch, float maxPitch) {
+        playSoundRandomPitch(target, soundEvent.get(), minPitch, maxPitch);
+    }
+
+    public static void playSoundRandomPitch(Entity target, Supplier<SoundEvent> soundEvent, float volume, float minPitch, float maxPitch) {
+        playSoundRandomPitch(target, soundEvent.get(), volume, minPitch, maxPitch);
+    }
 
     public static void playSoundRandomPitch(Entity target, SoundEvent soundEvent, float minPitch, float maxPitch) {
         playSound(target, soundEvent, minPitch, maxPitch);
@@ -17,6 +27,14 @@ public class SoundHelper {
         var random = target.getRandom();
         float pitch = RandomHelper.randomBetween(random, minPitch, maxPitch);
         playSound(target, soundEvent, volume, pitch);
+    }
+
+    public static void playSound(Entity target, Supplier<SoundEvent> soundEvent) {
+        playSound(target, soundEvent, 1f, 1f);
+    }
+
+    public static void playSound(Entity target, Supplier<SoundEvent> soundEvent, float volume, float pitch) {
+        playSound(target, soundEvent.get(), target.getSoundSource(), volume, pitch);
     }
 
     public static void playSound(Entity target, SoundEvent soundEvent) {
