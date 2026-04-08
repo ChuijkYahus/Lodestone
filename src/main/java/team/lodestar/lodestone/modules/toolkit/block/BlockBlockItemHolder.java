4e41222a10bd0df83365e8ffc19c3d8df64e61d2
SammySemicolon <malummoddevelopment@gmail.com>
2026-04-08 18:56:28 +0200

    Update BlockBlockItemHolder.java

--- src/main/java/team/lodestar/lodestone/modules/toolkit/block/BlockBlockItemHolder.java
+++ src/main/java/team/lodestar/lodestone/modules/toolkit/block/BlockBlockItemHolder.java
@@ -2,12 +2,13 @@ package team.lodestar.lodestone.modules.toolkit.block;
 
 
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.*;
 import net.neoforged.neoforge.registries.*;
 
 import java.util.function.*;
 
-public record BlockBlockItemHolder<T extends Block, K extends BlockItem>(DeferredHolder<Block, T> block, DeferredHolder<Item, K> item) implements Supplier<T> {
+public record BlockBlockItemHolder<T extends Block, K extends BlockItem>(DeferredHolder<Block, T> block, DeferredHolder<Item, K> item) implements Supplier<T>, ItemLike {
 
     @Override
     public T get() {
@@ -24,5 +25,10 @@ public record BlockBlockItemHolder<T extends Block, K extends BlockItem>(Deferre
 
     public DeferredHolder<Item, K> getItemHolder() {
         return item;
+    }
+
+    @Override
+    public Item asItem() {
+        return getItem();
     }
 }
