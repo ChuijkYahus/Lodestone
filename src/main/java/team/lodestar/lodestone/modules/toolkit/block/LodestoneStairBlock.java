af749b257a67d146cf503d26c1f98f940b1f3c70
SammySemicolon <malummoddevelopment@gmail.com>
2026-04-08 20:32:45 +0200

    Create LodestoneStairBlock.java

---
+++ src/main/java/team/lodestar/lodestone/modules/toolkit/block/LodestoneStairBlock.java
@@ -0,0 +1,24 @@
+package team.lodestar.lodestone.modules.toolkit.block;
+
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.StairBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.ItemAbility;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+public class LodestoneStairBlock extends StairBlock {
+    public LodestoneStairBlock(Properties properties) {
+        super(Blocks.AIR.defaultBlockState(), properties);
+    }
+
+    @Override
+    public float getExplosionResistance() {
+        return this.explosionResistance;
+    }
+}
