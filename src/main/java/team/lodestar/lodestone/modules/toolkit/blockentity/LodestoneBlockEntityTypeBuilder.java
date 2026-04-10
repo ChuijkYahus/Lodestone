f215021dd23996528d94a157c54c5ec600ce1bc1
SammySemicolon <malummoddevelopment@gmail.com>
2026-04-10 16:32:08 +0200

    Update LodestoneBlockEntityTypeBuilder.java

--- src/main/java/team/lodestar/lodestone/modules/toolkit/blockentity/LodestoneBlockEntityTypeBuilder.java
+++ src/main/java/team/lodestar/lodestone/modules/toolkit/blockentity/LodestoneBlockEntityTypeBuilder.java
@@ -5,20 +5,31 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import team.lodestar.lodestone.modules.toolkit.blockentity.LodestoneBlockEntityTicker.Type;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public final class LodestoneBlockEntityTypeBuilder<T extends LodestoneBlockEntity> {
     private final LodestoneBlockEntitySupplier<? extends T> factory;
-    final Set<Block> validBlocks;
+    private final Set<Supplier<? extends Block>> validBlocks;
     private Type type = Type.NONE;
 
-    private LodestoneBlockEntityTypeBuilder(LodestoneBlockEntitySupplier<? extends T> factory, Set<Block> validBlocks) {
+    private LodestoneBlockEntityTypeBuilder(LodestoneBlockEntitySupplier<? extends T> factory, Set<Supplier<? extends Block>> validBlocks) {
         this.factory = factory;
         this.validBlocks = validBlocks;
     }
 
-    public static <T extends LodestoneBlockEntity> LodestoneBlockEntityTypeBuilder<T> of(LodestoneBlockEntitySupplier<? extends T> factory, Block... validBlocks) {
+    public static <T extends LodestoneBlockEntity> LodestoneBlockEntityTypeBuilder<T> of(LodestoneBlockEntitySupplier<? extends T> factory, Supplier<? extends Block>... validBlocks) {
         return new LodestoneBlockEntityTypeBuilder<>(factory, ImmutableSet.copyOf(validBlocks));
+    }
+
+    public static <T extends LodestoneBlockEntity> LodestoneBlockEntityTypeBuilder<T> of(LodestoneBlockEntitySupplier<? extends T> factory, Block... validBlocks) {
+        Set<Supplier<? extends Block>> suppliers = new HashSet<>();
+        for (Block validBlock : validBlocks) {
+            suppliers.add(() -> validBlock);
+        }
+        return new LodestoneBlockEntityTypeBuilder<>(factory, ImmutableSet.copyOf(suppliers));
     }
 
     public LodestoneBlockEntityTypeBuilder<T> setTickerType(Type type) {
@@ -27,7 +38,8 @@ public final class LodestoneBlockEntityTypeBuilder<T extends LodestoneBlockEntit
     }
 
     public LodestoneBlockEntityType<T> build() {
-        return new LodestoneBlockEntityType<>(factory, validBlocks, type);
+        Set<Block> blocks = validBlocks.stream().map(Supplier::get).collect(Collectors.toSet());
+        return new LodestoneBlockEntityType<>(factory, blocks, type);
     }
 
     public interface LodestoneBlockEntitySupplier<T extends LodestoneBlockEntity> extends BlockEntityType.BlockEntitySupplier<T> {
