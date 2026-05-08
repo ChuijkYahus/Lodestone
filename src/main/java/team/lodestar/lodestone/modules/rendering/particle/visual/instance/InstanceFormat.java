bd28b242c897b2e259f485937513a7001b8c2946
SammySemicolon <malummoddevelopment@gmail.com>
2026-05-08 22:03:53 +0200

    Update InstanceFormat.java

--- src/main/java/team/lodestar/lodestone/modules/rendering/particle/visual/instance/InstanceFormat.java
+++ src/main/java/team/lodestar/lodestone/modules/rendering/particle/visual/instance/InstanceFormat.java
@@ -5,13 +5,21 @@ import java.util.List;
 
 public record InstanceFormat(List<InstanceElement> elements, int totalFloats, int strideBytes) {
 
+
+    public static Builder create() {
+        return new Builder();
+    }
+
     public static class Builder {
+
         private final List<InstanceElement> elements = new ArrayList<>();
         private int totalFloats = 0;
 
-        public Builder add(InstanceWriter writer) {
-            elements.add(new InstanceElement(writer));
-            totalFloats += writer.floatCount();
+        public Builder add(InstanceWriter... writers) {
+            for (InstanceWriter writer : writers) {
+                elements.add(new InstanceElement(writer));
+                totalFloats += writer.floatCount();
+            }
             return this;
         }
 
