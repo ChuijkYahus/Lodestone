efdfd5d3ffb7476bbac49b98ca6e7656e1501372
SammySemicolon <malummoddevelopment@gmail.com>
2026-05-09 20:42:31 +0200

    Update UnsafeBoundingBox.java

--- src/main/java/team/lodestar/lodestone/modules/toolkit/worldgen/UnsafeBoundingBox.java
+++ src/main/java/team/lodestar/lodestone/modules/toolkit/worldgen/UnsafeBoundingBox.java
@@ -36,9 +36,4 @@ public class UnsafeBoundingBox {
     public BoundingBox toBoundingBox() {
         return new BoundingBox(minX, minY, minZ, maxX, maxY, maxZ);
     }
-
-    //TODO: huh??
-    public boolean valid() {
-        return true;
-    }
 }
